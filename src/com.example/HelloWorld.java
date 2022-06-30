fa45df95e8eb761bab2995227a7e12b96ebd5f38
vlad-sheremet <uladzislau.sharamet@tui.co.uk>
2022-06-30 18:56:29 +0200

    Added message about git flow

--- src/com.example/HelloWorld.java
+++ src/com.example/HelloWorld.java
@@ -5,10 +5,13 @@ public class HelloWorld
    public static void main(String[] args)
    {
       System.out.println("Hello world!");
+
+      System.out.println("I know git branch flow");
    }
 
    public static void printMessage()
    {
       System.out.println("Hello message");
    }
+
 }
